3df088368d5d11ad314051397689a543d86fe119
Soren085 <giangtuan085@gmail.com>
2020-06-08 10:02:40 +0700

    Fix slideshow

--- src/app/home/components/screens/home-screen/parts/home-musicians/home-musicians.component.ts
+++ src/app/home/components/screens/home-screen/parts/home-musicians/home-musicians.component.ts
@@ -12,6 +12,8 @@ export class HomeMusiciansComponent implements OnInit {
   currentIndex: number = 0;
   mouseHoldPosition: number;
   isTransitionAllowed: boolean;
+  isDragging: boolean;
+  transformValue: string;
 
   constructor() { }
 
@@ -19,51 +21,72 @@ export class HomeMusiciansComponent implements OnInit {
     this.listMusician = [...Musicians];
 
     const newItem = this.listMusician[this.listMusician.length - 1];
+
+    // clone first and last item
     this.listMusician.push(this.listMusician[0]);
     this.listMusician.unshift(newItem);
     this.currentIndex = 1;
+    this.transform();
   }
 
-  public prev() {
+  public prev(): void {
     this.isTransitionAllowed = true;
     if (this.currentIndex > 0) {
       this.currentIndex--;
+      this.transform();
     }
   }
 
-
-  public next() {
+  public next(): void {
     this.isTransitionAllowed = true;
     if (this.currentIndex < this.listMusician.length - 1) {
       this.currentIndex++;
+      this.transform();
     }
   }
 
-
-  public transform() {
-    return `-${this.currentIndex * 100}%`;
+  private transform(value?: string) {
+    this.transformValue = `-${this.currentIndex * 100}`;
   }
 
-  public swipeHold(e) {
-    this.mouseHoldPosition = e.clientX;
-  }
-
-  public swipeRelease(e): void {
-    if (this.mouseHoldPosition < e.clientX) {
-      this.prev();
-    } else if (this.mouseHoldPosition > e.clientX) {
-      this.next();
+  public refreshPosition(index: number): void {
+    if (index !== 0) {
+      return;
     }
-  }
-
-  public refreshPosition(): void {
     if (this.currentIndex === 0) {
       this.isTransitionAllowed = false;
       this.currentIndex = this.listMusician.length - 2;
     } else if (this.currentIndex === this.listMusician.length - 1) {
       this.isTransitionAllowed = false;
       this.currentIndex = 1;
+    } else {
+      return;
     }
+    this.transform();
+  }
+
+  public swipeAction(e): void {
+    if (this.isDragging) {
+      console.log(e, typeof e);
+    }
+    // posX2 = posX1 - e.clientX;
+    // posX1 = e.clientX;
+    // items.style.left = (items.offsetLeft - posX2) + "px";
+  }
+
+  public swipeRelease(e): void {
+    this.isDragging = false;
+    console.log(e, typeof e);
+
+    // posFinal = items.offsetLeft;
+    // if (posFinal - posInitial < -threshold) {
+    //   shiftSlide(1, 'drag');
+    // } else if (posFinal - posInitial > threshold) {
+    //   shiftSlide(-1, 'drag');
+    // } else {
+    //   items.style.left = (posInitial) + "px";
+    // }
+
   }
 
 
